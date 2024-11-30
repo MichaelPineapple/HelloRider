51ffd3b9a65880771898bd6d77ace9fecbe9c94b
MclGaming <meowclewizlon@gmail.com>
2024-11-30 12:43:56 -0800

    Save

---
+++ HelloRider/Main.cs
@@ -0,0 +1,10 @@
+namespace HelloRider;
+using OpenTK;
+
+public static class Main
+{
+    public static void main()
+    {
+        Console.WriteLine("Hello, Rider!");
+    }
+}
---
+++ HelloRider/Program.cs
@@ -0,0 +1,3 @@
+﻿using HelloRider;
+
+Main.main();
